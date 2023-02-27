820c68ceebc6c05d13f3b01d6d1f4081c1d968be
Inna Nomerovkina <inkosof@gmail.com>
2023-02-28 01:12:36 +0200

    task -02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,34 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const firstItemEl = document.createElement('li');
+firstItemEl.textContent = 'Potatoes';
+firstItemEl.classList.add('item');
+
+const secondItemEl = document.createElement('li');
+secondItemEl.textContent = 'Mushrooms';
+secondItemEl.classList.add('item');
+
+const thirdItemEl = document.createElement('li');
+thirdItemEl.textContent = 'Garlic';
+thirdItemEl.classList.add('item');
+
+const fourthItemEl = document.createElement('li');
+fourthItemEl.textContent = 'Tomatos';
+fourthItemEl.classList.add('item');
+
+const fifthItemEl = document.createElement('li');
+fifthItemEl.textContent = 'Herbs';
+fifthItemEl.classList.add('item');
+
+const sixthItemEl = document.createElement('li');
+sixthItemEl.textContent = 'Condiments';
+sixthItemEl.classList.add('item');
+
+const listIngredientsEl = document.querySelector('#ingredients');
+listIngredientsEl.append(firstItemEl, secondItemEl, thirdItemEl, fourthItemEl, fifthItemEl, sixthItemEl);
+
+
+
+
