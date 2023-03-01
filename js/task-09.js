bc58e71aaa093c8b8a25eaa77fc3505184e849d0
Inna Nomerovkina <inkosof@gmail.com>
2023-03-01 18:00:35 +0200

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const bodyEl = document.querySelector('body');
+const colorEl = document.querySelector('.color');
+
+bodyEl.addEventListener('click', onColorChange)
+
+function onColorChange(event) {
+ const  hexColor = getRandomHexColor();
+  event.currentTarget.style.backgroundColor = hexColor;
+  colorEl.textContent = hexColor;
+  
+}
