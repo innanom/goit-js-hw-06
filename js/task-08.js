d6a20a4cbc58215d8d605052ff13f48aaf81b54c
Inna Nomerovkina <inkosof@gmail.com>
2023-03-01 17:12:27 +0200

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,27 @@
+const formEl = document.querySelector('.login-form');
+console.log(formEl);
 
+
+formEl.addEventListener('submit', onFormElSubmit);
+
+function onFormElSubmit(event) {
+    event.preventDefault();
+    
+    const {
+    elements: { email, password }
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    // return console.log("Всі поля повинні бути заповнені");
+      return alert("Всі поля повинні бути заповнені");
+  }
+
+  const data = { 
+   [ email.name ]: email.value, 
+   [ password.name ]: password.value 
+};
+
+    console.log(data);
+
+    event.target.reset();
+}
